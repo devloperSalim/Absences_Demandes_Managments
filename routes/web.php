f230bc9cf4c65acf2a3bc9ca1539f382d5a7b5d7
LAHCINI ABDELHAQ <lahciniabdelhaq@gmail.com>
2024-03-15 16:54:23 +0100

    change

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,17 @@ Route::get('/', function () {
 });
 
 // Route::prefix('admin')->middleware('auth')->group(function () {
-    Route::get('/home', function () {
-        return view('home');
-    });
+//     Route::get('/home', function () {
+//         return view('home');
+//     });
 
-    Route::get('/stagaires', [StagaireController::class, 'index'])->name('stagaires.index');
-    Route::get('/stagaires/create', [StagaireController::class, 'create'])->name('stagaires.create');
-    Route::post('/stagaires', [StagaireController::class, 'store'])->name('stagaires.store');
-    Route::get('/stagaires/{registration_number}', [StagaireController::class, 'show'])->name('stagaires.show');
-    Route::get('/stagaires/{registration_number}/edit', [StagaireController::class, 'edit'])->name('stagaires.edit');
-    Route::put('/stagaires/{registration_number}', [StagaireController::class, 'update'])->name('stagaires.update');
-    Route::delete('/stagaires/{registration_number}',[StagaireController::class,'destroy'])->name('stagaires.destroy');
+//     Route::get('/stagaires', [StagaireController::class, 'index'])->name('stagaires.index');
+//     Route::get('/stagaires/create', [StagaireController::class, 'create'])->name('stagaires.create');
+//     Route::post('/stagaires', [StagaireController::class, 'store'])->name('stagaires.store');
+//     Route::get('/stagaires/{registration_number}', [StagaireController::class, 'show'])->name('stagaires.show');
+//     Route::get('/stagaires/{registration_number}/edit', [StagaireController::class, 'edit'])->name('stagaires.edit');
+//     Route::put('/stagaires/{registration_number}', [StagaireController::class, 'update'])->name('stagaires.update');
+//     Route::delete('/stagaires/{registration_number}',[StagaireController::class,'destroy'])->name('stagaires.destroy');
 // });
 
 
